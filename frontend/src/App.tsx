e20feb5f53b7335db6df600850ccceacda8e67a9
Emily Reyes <emilymwind1@gmail.com>
2023-03-23 00:50:05 -0600

    Final changes + css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import teams from './CollegeBasketballTeams.json';
+
+const Teams = teams['teams']
+
+function Welcome(){
+  return(<h1>Information about all the colleges in NCAA Basketball</h1>)
+}
+
+function Thanks(){
+  return(<h3>Thank you for visiting our website.</h3>)
+}
+
+class Team extends React.Component <any,any>{
+  render() {
+    
+    const oneTeam = this.props;
+    
+    return(
+    <div className="Team">
+        <br></br>
+        <h2>{oneTeam.school}</h2>
+        <h3>Mascot: {oneTeam.name}</h3>
+        <h3>Location: {oneTeam.city}, {oneTeam.state}</h3>
+    </div>
+    );
+  }
+}
+
+function TeamList(){
+  return(
+  <div>
+      {Teams.map(oneTeam => <Team {...oneTeam}/>)}
+  </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Emily's Website</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+      <Welcome/>
+      <TeamList/>
+      <Thanks/>
+  </div>
   );
 }
+
 
 export default App;
